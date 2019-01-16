5a86d7c90a820816e8c5686480cdaa42ef4f57b8
jesseleejohnston <jesseleejohnston@gmail.com>
2019-01-15 21:38:44 -0800

    Added faker data.

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,9 +1,27 @@
 <template>
   <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <h1>About Info</h1>
+    <ul>
+      <li v-for="(item, index) in $page.allFaker.edges" :key="index"> {{ item.node.author }} </li>
+    </ul>
   </Layout>
 </template>
+
+<page-query>
+{
+  allFaker (perPage: 5){
+    edges {
+      node {
+        author
+        email
+        avatar
+      }
+    }
+  }
+}
+</page-query>
+
+
 
 <script>
 export default {}
